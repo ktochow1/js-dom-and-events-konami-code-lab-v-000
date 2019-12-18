e35d4af945ed8231ae598bc5d42378665fe0028c
Kat Tochowicz <ktochow1@binghamton.edu>
2019-12-18 17:34:09 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
 
 function init() {
+  var index = 0; //index needs to be declared inside function
   // Write your JavaScript code inside the init() function
-
+  document.body.addEventListener('keydown', function(e){
+    const key = parseInt(e.which || e.detail);
+      if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert("you did it!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
+init(); //need to call function in the end
